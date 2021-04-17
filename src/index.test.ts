6abc88a017a8867b264c2461944f7e9067edf4b2
Stefan Natter <stefan@natter.at>
2021-04-17 12:55:48 +0200

    feat: detect current commit hash or tag

--- src/index.test.ts
+++ src/index.test.ts
@@ -3,7 +3,7 @@
  * @see https://fireflysemantics.medium.com/unit-testing-commander-scripts-with-jest-bc32465709d6
  */
 import path from 'path'
-import { exec, ExecException } from 'child_process'
+import { exec, execSync, ExecException } from 'child_process'
 
 describe('CLI', () => {
   function cli(args: any, cwd: any) {
@@ -28,9 +28,40 @@ describe('CLI', () => {
     })
   }
 
-  it('hello command should return "hello" in the console', async () => {
-    const result = await cli(['hello'], '.')
+  beforeEach(() => {
+    try {
+      execSync('git tag -d 99.99.99 2> /dev/null')
+    } catch (err) {
+      // e.g. tag does not exist yet
+    }
+  })
+
+  afterAll(() => {
+    execSync('git tag -d 99.99.99')
+  })
+
+  it('current-version command should return current commit hash in the console', async () => {
+    const result = await cli(['current-version'], '.')
     expect(result.code).toStrictEqual(0)
-    expect(result.stdout).toStrictEqual('hello\n')
+    expect(result.stdout.toString()).toHaveLength(7)
+  })
+
+  it('current-version command should return current version in the console', async () => {
+    execSync('git tag 99.99.99')
+
+    const result = await cli(['current-version'], '.')
+    expect(result.code).toStrictEqual(0)
+    expect(result.stdout).toStrictEqual('99.99.99')
+  })
+
+  it('returns tag not commit hash when both are present', async () => {
+    execSync('git commit -m "empty" --allow-empty')
+    execSync('git tag 99.99.99')
+
+    const result = await cli(['current-version'], '.')
+    expect(result.code).toStrictEqual(0)
+    expect(result.stdout).toStrictEqual('99.99.99')
+
+    execSync('git reset HEAD~1')
   })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,30 @@
 #!/usr/bin/env node
 
+import { execSync } from 'child_process'
+
 import { Command } from 'commander'
+
+// inspired by https://gist.github.com/mjj2000/3ee188cc155c26a118b06116ad0ebd1d
+const gitCommand =
+  'git describe --exact-match --tags 2> /dev/null || git rev-parse --short HEAD'
 
 const program = new Command()
 
 /** **************************************** */
 
-// Print hello to console
-// $ ns-script hello
+// Print current version
+// $ ns-script current-version
+// $ ns-script cv
 program
-  .command('hello') // sub-command name
-  .description('Say hello') // command description
+  .command('current-version') // sub-command name
+  .alias('cv') // alias name
+  // command description
+  .description(
+    'Returns the short hash of the last commit or the current version',
+  )
   // function to execute when command is uses
   .action(() => {
-    console.log('hello')
+    process.stdout.write(execSync(gitCommand).toString().trim())
   })
 
 // allow commander to parse `process.argv`
