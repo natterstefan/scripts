2ae715c23422aff1f7b83d813843f2052926a30d
Stefan Natter <stefan@natter.at>
2021-04-18 15:00:12 +0200

    fix: added @tsconfig/node14, main in package.json and requires min. node@14

--- bin/index.ts
+++ bin/index.ts
@@ -1,8 +1,13 @@
 #!/usr/bin/env node
+// 👆 Used to tell Node.js that this is a CLI tool
 
 /* eslint-disable global-require */
 /* eslint-disable import/no-dynamic-require */
 /* eslint-disable @typescript-eslint/no-var-requires */
+
+/**
+ * TODO: add figlet (https://itnext.io/how-to-create-your-own-typescript-cli-with-node-js-1faf7095ef89)
+ */
 import { Command } from 'commander'
 
 const program = new Command()
